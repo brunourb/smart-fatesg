b0343bdff622af26a8bcbe29a13d13ea634cb08e
Bruno Urbano Rodrigues <brunourb@gmail.com>
2022-09-21 22:00:45 -0300

    feature(arch): create initial setup

---
+++ src/main/java/br/com/senaigo/fatesg/smartfatesg/FatesgApplication.java
@@ -0,0 +1,12 @@
+package br.com.senaigo.fatesg.smartfatesg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatesgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatesgApplication.class, args);
+    }
+}
